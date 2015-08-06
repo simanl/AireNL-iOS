8f54a8f0c6940f2d6a570595942697430b4c0565
Daniel Lozano <danlozano@gmail.com>
2015-08-06 17:50:48 -0500

    UI for watchkit app

---
+++ AireNL WatchKit Extension/SecondaryInterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  SecondaryInterfaceController.h
+//  AireNL
+//
+//  Created by Daniel Lozano on 8/6/15.
+//  Copyright (c) 2015 Icalia Labs. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface SecondaryInterfaceController : WKInterfaceController
+
+@end
---
+++ AireNL WatchKit Extension/SecondaryInterfaceController.m
@@ -0,0 +1,36 @@
+//
+//  SecondaryInterfaceController.m
+//  AireNL
+//
+//  Created by Daniel Lozano on 8/6/15.
+//  Copyright (c) 2015 Icalia Labs. All rights reserved.
+//
+
+#import "SecondaryInterfaceController.h"
+
+@interface SecondaryInterfaceController ()
+
+@end
+
+@implementation SecondaryInterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+    
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
