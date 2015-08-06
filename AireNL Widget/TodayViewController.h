075d2d98dbd1278f4e465c4ba079e5bc870dacdd
Daniel Lozano <danlozano@gmail.com>
2015-08-05 20:18:30 -0500

    Today extension widget

---
+++ AireNL Widget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  AireNL Widget
+//
+//  Created by Daniel Lozano on 8/5/15.
+//  Copyright (c) 2015 Icalia Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
