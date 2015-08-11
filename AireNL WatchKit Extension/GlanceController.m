5ec98ee98041bbfc2e4d3be0a965bc9873cd60b6
Daniel Lozano <danlozano@gmail.com>
2015-08-11 16:13:54 -0500

    Finished with glance

--- AireNL WatchKit Extension/GlanceController.m
+++ AireNL WatchKit Extension/GlanceController.m
@@ -8,18 +8,21 @@
 
 #import "GlanceController.h"
 
+#import "CurrentResults.h"
 
 @interface GlanceController()
 
-@end
+@property (nonatomic) CurrentResults *currentResults;
 
+@end
 
 @implementation GlanceController
 
-- (void)awakeWithContext:(id)context {
+- (void)awakeWithContext:(id)context
+{
+    // Configure interface objects here.
     [super awakeWithContext:context];
 
-    // Configure interface objects here.
 }
 
 - (void)willActivate {
@@ -30,6 +33,35 @@
 - (void)didDeactivate {
     // This method is called when watch view controller is no longer visible
     [super didDeactivate];
+}
+
+#pragma mark - Network
+
+- (void)loadAssets
+{
+    CurrentResults *currentResults = [[CurrentResults alloc] init];
+    currentResults.date = [NSDate date];
+    currentResults.temperature = @(100);
+    currentResults.wind = @(500);
+    
+    ImecaResults *imecaResults = [[ImecaResults alloc] init];
+    imecaResults.amount = @(68);
+    imecaResults.quality = @"Muy Mala";
+    currentResults.imeca = imecaResults;
+    
+    ResultLocation *location = [[ResultLocation alloc] init];
+    location.cityName = @"Monterrey";
+    location.areaName = @"Estacion Centro Obispado de Nuevo Leon";
+    currentResults.location = location;
+    
+    self.currentResults = currentResults;
+    
+    [self updateScreen];
+}
+
+- (void)updateScreen
+{
+    [self setTitle: self.currentResults.location.cityName];
 }
 
 @end
